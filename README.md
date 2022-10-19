8f44cc9e1c1515c256634904c1f03664922d5347
garcia0216 <chrislin0729@outlook.com>
2022-10-19 13:35:36 -0400

    added readme draft screenshot picture and comments to js file

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # js-password-generator
+
+1. A description of your application
+
+2. Screenshot of your application (Maybe a screenshot demonstrated your password)
+
+
+3. Deployed link to your application (GitHub pages)
+
+<!-- A full built README would typically contain a Table of Contents, Title, Description, Installation, Usage (How to use), Technologies Used, Links, Contact -->
--- assets/script.js
+++ assets/script.js
@@ -5,30 +5,30 @@
 
 // Need a way to store the user's choice for length of password --> variable
 // Prompt user for length of password
-  // IF the user chooses a number outside the acceptable range, need to do SOMETHING
-    // Alert that the validation failed?
-    // Return to the prompt, OR
-    // Exit out of the function
+// IF the user chooses a number outside the acceptable range, need to do SOMETHING
+// Alert that the validation failed?
+// Return to the prompt, OR
+// Exit out of the function
 // Need variables storing each of our character sets--arrays?:
-  // Uppercase
-  // Lowercase
-  // Numbers
-  // Special Characters
+// Uppercase
+// Lowercase
+// Numbers
+// Special Characters
 // Need a variable to store the user's choice for whether to use each character set
 // IF the user fails to select any characters, need to do SOMETHING
-  // Alert that the validation failed?
-  // Return to the prompt, OR
-  // Exit out of the function
+// Alert that the validation failed?
+// Return to the prompt, OR
+// Exit out of the function
 // Generate a random character from each chosen array
-  // May be some IF statements regarding which character arrays were chosen
-  // Generate a random array index using Math.floor(Math.random() * array.length)
-  // Grab the character at the randomly-generated index
-  // Add it to the end of the password string
-    // Need a variable to store the generated password
+// May be some IF statements regarding which character arrays were chosen
+// Generate a random array index using Math.floor(Math.random() * array.length)
+// Grab the character at the randomly-generated index
+// Add it to the end of the password string
+// Need a variable to store the generated password
 // Repeat ^ for each chosen array
 // Join/concatenate the up to four selected character sets
 // Iterate until password.length === the length the user chose
-  // Generate a random character from the mega-array and add it to the end of the password string on each loop
+// Generate a random character from the mega-array and add it to the end of the password string on each loop
 // Return the generated password
 
 
@@ -37,56 +37,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword(){
+function generatePassword() {
 
-var passwordLength = prompt('Password length must be between 8 and 128 characters?')
+  var passwordLength = prompt('Password length must be between 8 and 128 characters?')
 
-if (passwordLength >=8 && passwordLength <=128){
-  alert('Valid password length! Please choose characters')
-// if (passwordLength.length >= 128){
-// prompt('invalid password length!')
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    alert('Valid password length! Please choose characters')
+    // if (passwordLength.length >= 128){
+    // prompt('invalid password length!')
 
-}else {
-  alert('Invalid password length! Try again')
-  return
-}
+  } else {
+    alert('Invalid password length! Try again')
+    return
+  }
+
+  // confirm returns a true or false
+  var isUpperCase = confirm('Do you want uppercase letters?')
+  var isLowerCase = confirm('Do you want lowercase letters?')
+  var isNumber = confirm('Do you want numbers?')
+  var specialCharacters = confirm('Do you want special characters?')
+  var characterOptions = '';
+
+   
 
 
-var isUpperCase = confirm('Do you want uppercase letters?')
-var isLowerCase = confirm('Do you want lowercase letters?')
-var isNumber = confirm ('Do you want numbers?')
-var specialCharacters = confirm('Do you want special characters?')
-var characterOptions = '';
+  // password options
+  // our conditional is only applying the characters if our confirm returns true
+  if (isUpperCase) {
+    characterOptions += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  }
+  if (isLowerCase) {
+    characterOptions += 'abcdefghijklmnopqrstuvwxyz'
+  }
+  if (specialCharacters) {
+    characterOptions += '!@#$%&\'()*+,^-./:;<=>?[]_`{~}|'
+  }
+  if (isNumber) {
+    characterOptions += '123456789'
+  }
 
+  var password = '';
 
-
-if (isUpperCase){
-  characterOptions +='ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-}
-if (isLowerCase){
-  characterOptions +='abcdefghijklmnopqrstuvwxyz'
-}
-if (specialCharacters){
-  characterOptions +='!@#$%&\'()*+,^-./:;<=>?[]_`{~}|'
-}
-if (isNumber){
-characterOptions +='123456789'
-}
-
-var password = '';
-
-for(var i = 0; i < passwordLength; i=i+1){
-  // Math.floor(Math.random() * array.length)
-var randomCharacter = characterOptions[Math.floor(Math.random() * characterOptions.length)]
-password += randomCharacter
-}
-return password
+  for (var i = 0; i < passwordLength; i++) {
+    // Math.floor(Math.random() * array.length)
+    var randomNum = Math.floor(Math.random() * characterOptions.length);
+    var randomCharacter = characterOptions[randomNum];
+    password += randomCharacter;
+  }
+  return password;
 }
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  if (!password){
-  return
+  if (!password) {
+    return
   }
   var passwordText = document.querySelector("#password");
 
